6e1865d8d5d05ce634876a6c657f5a1d030b59ad
costa <byxu@yilesi.cn>
2023-08-04 13:58:49 +0800

    feat: 增加tab组件带图标使用方法

---
+++ assets/chunks/Container.3268ccaa.js
@@ -0,0 +1,1 @@
+import{_ as s,v as _,b as n,z as t}from"./framework.5a619fb0.js";const a={},o={class:"container"};function c(e,r){return _(),n("div",o,[t(e.$slots,"default",{},void 0,!0)])}const i=s(a,[["render",c],["__scopeId","data-v-96a75348"]]);export{i as C};
